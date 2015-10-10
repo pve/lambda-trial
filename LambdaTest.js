671de67672da4cd670620d74275c2183e92fd66e
Peter <pveijk@gmail.com>
2015-10-10 19:34:57 +0200

    Update LambdaTest.js

--- LambdaTest.js
+++ LambdaTest.js
@@ -3,6 +3,7 @@ console.log('Loading event again');
 
 exports.handlers = function(event, context) {
     console.log('Who = ' + event.key1);
+    console.log('Who = ' + event.key1);
     console.log('is = ' + event.key2);
     console.log('awesome = ' + event.key3);
     context.done(null, 'Hello World');  // SUCCESS with message
